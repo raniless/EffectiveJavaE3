e80941310676d91d8916607eaa3b13b2ebc1c877
raniless <raniless@naver.com>
2020-08-27 21:46:19 +0900

    Summary > .md 파일로 변경

--- src/chapter04/item15/Item15.java
+++ src/chapter04/item15/Item15.java
@@ -4,16 +4,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-// Title : 클래스와 멤버의 접근 권한을 최소화 하라
 public class Item15 {
-    /*
-     - 모든 클래스와 멤버의 접근성을 가능한 한 좁혀야 한다.
-     - public 클래스의 인스턴스 필드는 되도록 public이 아니어야 한다.
-     - public 가변 필드를 갖는 클래스는 일반적으로 스레드 안전하지 않다.
-     - 클래스에서 public static final 배열 필드를 두거나 이 필드를 반환하는 접근자 메서드를 제공해서는 안된다.
-       (Why? > 길이가 0이 아닌 배열은 모두 변경 가능)
-     */
-
     //보안 허점이 숨어 있다.
     public static final String[] VALUE = {"a", "b", "c", "d"};
 
--- src/chapter04/item16/Item16.java
+++
@@ -1,9 +0,0 @@
-package chapter04.item16;
-
-// Title : public 클래스에서는 public 필드가 아닌 접근자 메서드를 사용하라
-public class Item16 {
-    /*
-     - public 클래스는 절대 가변 필드를 직접 노출해서는 안된다.
-     - 패키지 바깥에서 접근할 수 있는 클래스라면 접근자를 제공
-     */
-}
